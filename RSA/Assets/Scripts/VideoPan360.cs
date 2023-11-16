673796ee6fa1d59e8bdaeda82bdeee1898b1b195
RadJohnson <radjohnson77@gmail.com>
2023-11-16 16:10:31 +0000

    Feat: added inverted sphere for playing videos

---
+++ RSA/Assets/Scripts/VideoPan360.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPan360 : MonoBehaviour
+{
+    //Make a thing wqhere right or lecfft mouse click for dragging camerea to pan roudn 360 video
+
+    void Update()
+    {
+        
+    }
+}
