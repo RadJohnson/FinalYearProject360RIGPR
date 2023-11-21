2dfc9dc5e075009338267284feef7c17dd060d34
RadJohnson <radjohnson77@gmail.com>
2023-11-21 07:33:34 +0000

    Chore: added old unity input mouse inputs for  panning camera

--- RSA/Assets/Scripts/VideoPan360.cs
+++ RSA/Assets/Scripts/VideoPan360.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 
 public class VideoPan360 : MonoBehaviour
 {
     //Make a thing wqhere right or lecfft mouse click for dragging camerea to pan roudn 360 video
-
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //do rotation stuff here
+
+            Input.GetAxis("Mouse X");
+            Input.GetAxis("Mouse Y");
+        }
     }
 }
