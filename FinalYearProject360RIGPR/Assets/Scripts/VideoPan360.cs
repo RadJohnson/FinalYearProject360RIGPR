6f88718f821374fbb232c6efb96efc8f2d860cb3
RadJohnson <radjohnson77@gmail.com>
2023-11-23 15:55:09 +0000

    feat: camera can now rotate
    also increased the size of the sphere that has the video play on it to 100x100x100 and rotated it to -90 on the X-axis as a temporary fix while setting project up

--- FinalYearProject360RIGPR/Assets/Scripts/VideoPan360.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/VideoPan360.cs
@@ -1,19 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class VideoPan360 : MonoBehaviour
 {
-    //Make a thing wqhere right or lecfft mouse click for dragging camerea to pan roudn 360 video
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //do rotation stuff here
+    private Vector2 cameraRotation;
+    [SerializeField, Tooltip("Y is minimum camera angle and Y is maximum camera angle")] private Vector2 cameraClampYAxis;
 
-            Input.GetAxis("Mouse X");
-            Input.GetAxis("Mouse Y");
+    void Update()//may need to clamp the mouse to the window
+    {
+        if (Input.GetMouseButton(0))// hold left mouse to allow fo thte inputs to work
+        {
+            cameraRotation += new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+            cameraRotation.y = Mathf.Clamp(cameraRotation.y, cameraClampYAxis.x, cameraClampYAxis.y);
         }
+
+        transform.localRotation = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0f);
+    }
+
+
+    private void Reset()
+    {
+        cameraClampYAxis = new Vector2(-90f, 90f);
     }
 }
