b4ef047922974e17a9742f7ea7bf146a422585dd
JamsterGuy <rcjamierussell@gmail.com>
2023-11-23 15:59:17 +0000

    Feat: Added btn skeleton functions - Exit, Open Manual

---
+++ FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/MainMenuUIScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load Videos From File (For Loop)
+        // For each video file create a ui component
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    
+    }
+
+
+    // Exit Button Functionality
+    public void QuitApplication()
+    {
+        Application.Quit(); // Closes Application
+    }
+
+    // Open User Manual
+    public void OpenUserManual()
+    {
+
+    }
+
+            // Loading Video Files
+    
+
+
+            // Choosing Video File
+
+    //Choose Video from list
+    public void ChooseVideo()
+    {
+
+    }
+
+    // Open Other Video File
+    public void OpenOtherVideoFile()
+    {
+
+    }
+
+
+
+
+}
