11278ec882da54866cf4a64c3a55fbd42b6645cb
RadJohnson <radjohnson77@gmail.com>
2023-11-23 15:08:38 +0000

    Fix: Resolved merge conflict and recovered menu scene

--- FinalYearProject360RIGPR/Assets/Scripts/VideoPan360.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/VideoPan360.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
+
 
 public class VideoPan360 : MonoBehaviour
 {
