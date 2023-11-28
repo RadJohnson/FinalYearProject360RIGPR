c0b5eca02fe9cf0ffb87c1e0961431edb20a7161
JamsterGuy <93982991+JamsterGuy@users.noreply.github.com>
2023-11-28 14:56:06 +0000

    Feat: Code to search through video file folder

--- FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/OpenFile.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/OpenFile.cs
@@ -1,67 +1,52 @@
 using AnotherFileBrowser.Windows;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
+
 public class OpenFile : MonoBehaviour
 {
     public RawImage rawImage;
+    public string VideoFolderpath = @"C:\TestVideoFolder"; // Video Folder Path 
     
-    // Video stuff
-    public VideoPlayer videoPlayer;
-    public string videoURL;
+    void Start()
+    {
+
+
+        DirectoryInfo dir = new DirectoryInfo(VideoFolderpath);
+        FileInfo[] info = dir.GetFiles("*.*");
+        foreach (FileInfo f in info)
+        {
+            Debug.Log(f.Extension);
+        }
+
+
+    }
 
 
     public void OpenFileBrowser()
     {
-    
-       var bp = new BrowserProperties();
-       string path;
-       bp.filter = "Video files | *.mp4; *.AVI";
-       //bp.filter = "Image files | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
-       bp.filterIndex = 0;
 
-       new FileBrowser().OpenFileBrowser(bp, path =>
-       {
-        StartCoroutine(LoadImage(path)); 
-       });
-    
+        var bp = new BrowserProperties();
+        string path;
+        bp.filter = "Video files | *.mp4; *.AVI";   // FILTER TO ONLY SEE VIDEOS IN EXPLORER
+        bp.filterIndex = 0;
 
-    IEnumerator LoadImage(string path)
-    {
-        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(path) )
+        new FileBrowser().OpenFileBrowser(bp, path =>
         {
-
-            yield return uwr.SendWebRequest();
-
-            if(uwr.isNetworkError || uwr.isHttpError)
-            {
-                Debug.Log(uwr.error);
-            }
-            else
-            {
-                //var uwrTexture = DownloadHandlerTexture.GetContent(uwr);
-                //rawImage.texture = uwrTexture;
-                VideoHandling(path);
-            }
-
-        }
+            Debug.Log(path);
+        });
 
     }
-    }
 
-        public void VideoHandling(string path)
-    {
-        videoPlayer.url = path;
-        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
-        videoPlayer.EnableAudioTrack (0, true);
-        videoPlayer.Prepare ();
-    }
 
-  
+
+
 
 
 }
