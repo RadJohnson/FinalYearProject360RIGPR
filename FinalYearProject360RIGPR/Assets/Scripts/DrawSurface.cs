42908b4a6c0751e9b7115fa5b25cd0bc7555fdeb
RadJohnson <radjohnson77@gmail.com>
2023-11-30 22:42:54 +0000

    !fix: drawing sphere is completely transparent
    does need the drawing to disengage completely when starting and stopping drawing and might need to see if it is possible to fix an issue where it draws badly when going from one side of the texture to another

--- FinalYearProject360RIGPR/Assets/Scripts/DrawSurface.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/DrawSurface.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class DrawSurface : MonoBehaviour
 {
     [SerializeField] internal Texture2D texture;
-    [SerializeField] internal Vector2Int textureSize = new Vector2Int(2048*2, 2048*2);
+    [SerializeField] internal Vector2Int textureSize /*= new Vector2Int(2048, 2048)*/;
     [SerializeField] internal Color[] initialColour = new Color[1];
 
     void Start()
@@ -15,22 +15,23 @@ public class DrawSurface : MonoBehaviour
     
         texture = new Texture2D(textureSize.x, textureSize.y);
 
-        initialColour = Enumerable.Repeat(new Color(1,1,1,0), textureSize.x * textureSize.y).ToArray();
+        initialColour = Enumerable.Repeat(new Color(0,0,0,0), textureSize.x * textureSize.y).ToArray();
         texture.SetPixels(initialColour);
         initialColour = new Color[1];
+        texture.Apply();
         r.material.mainTexture = texture;
     }
 
-    void Reset()
-    {
-        initialColour[0] = Color.clear;
-
-        var r = gameObject.GetComponent<Renderer>();
-
-        texture = new Texture2D(textureSize.x, textureSize.y);
-        //texture.SetPixels(initialColour);
-
-        r.material.mainTexture = texture;
-    }
+    //void Reset()
+    //{
+    //    initialColour[0] = Color.clear;
+    //
+    //    var r = gameObject.GetComponent<Renderer>();
+    //
+    //    texture = new Texture2D(textureSize.x, textureSize.y);
+    //    //texture.SetPixels(initialColour);
+    //
+    //    r.material.mainTexture = texture;
+    //}
 
 }
