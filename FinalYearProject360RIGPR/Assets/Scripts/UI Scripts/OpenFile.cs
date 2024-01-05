f1ce99ab99af72e82966a4c2c8aec65209c92454
up2056252 <147083370+up2056252@users.noreply.github.com>
2024-01-05 15:58:57 +0000

    Fix: Fixed open other button not actually playing the video selected

--- FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/OpenFile.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/OpenFile.cs
@@ -15,13 +15,22 @@ public class OpenFile : MonoBehaviour
 {
     public RawImage rawImage;
     private string VideoFolderpath = @"C:\TestVideoFolder"; // Video Folder Path 
-
+    public string path;
     public GameObject VideoUIIcon;
     public GameObject VideoGrid;
 
+    public string VideoFileURL; // Video File URL
+    public string VideoFileName;
+    private Button button;
+    [SerializeField] internal VideoPlayer videoPlayer;
+
+
+    private void Awake()
+    {
+        videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
+    }
     void Start()
     {
-
 
         DirectoryInfo dir = new DirectoryInfo(VideoFolderpath);     // Create info for the given video folder path
         FileInfo[] info = dir.GetFiles("*.*");                // store files in folder as an array called fileinfo
@@ -52,14 +61,21 @@ public class OpenFile : MonoBehaviour
     {
 
         var bp = new BrowserProperties();
-        string path;
+        //string path;
         bp.filter = "Video files | *.mp4; *.AVI";   // FILTER TO ONLY SEE VIDEOS IN EXPLORER
         bp.filterIndex = 0;
-
         new FileBrowser().OpenFileBrowser(bp, path =>
         {
             Debug.Log(path);
+            videoPlayer.url = "file://" + path;
+            Debug.Log(videoPlayer.url);
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+            videoPlayer.EnableAudioTrack(0, true);
+            videoPlayer.Prepare();
+            videoPlayer.Play();
+
         });
+
 
     }
 
