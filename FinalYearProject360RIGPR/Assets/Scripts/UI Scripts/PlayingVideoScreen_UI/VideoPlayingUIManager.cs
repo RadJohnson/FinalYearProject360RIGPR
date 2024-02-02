e489b2026f5512c08a3aced3e2c801d91ecf4ddc
up2056252 <147083370+up2056252@users.noreply.github.com>
2024-02-02 17:41:09 +0000

    Feat: Added play button sprite and coded some more button functionality

--- FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/PlayingVideoScreen_UI/VideoPlayingUIManager.cs
+++ FinalYearProject360RIGPR/Assets/Scripts/UI Scripts/PlayingVideoScreen_UI/VideoPlayingUIManager.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoPlayingUIManager : MonoBehaviour
 {
 
+    // VideoPlayer
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    // Buttons
     [SerializeField] private Button exitBtn;
+    [SerializeField] private Button pauseplayBtn;
+    [SerializeField] private Button restartBtn;
+    [SerializeField] private Button skipfwdBtn;
+    [SerializeField] private Button skipbwdBtn;
+    [SerializeField] private Button bookmarksBtn;
+
+    public bool isBookmarksOpen;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        exitBtn.onClick.AddListener(Exit_Video);
+
+        exitBtn.onClick.AddListener(Exit_Video); // Exit button
+        pauseplayBtn.onClick.AddListener(Play_Pause); // Play pause button
+        bookmarksBtn.onClick.AddListener(OpenCloseBookmarksTab); // Open Bookmarks Tab
+
+        isBookmarksOpen = false;
+
     }
 
     // Update is called once per frame
@@ -25,10 +43,49 @@ public class VideoPlayingUIManager : MonoBehaviour
 
     public void Play_Pause()
     {
+        if(videoPlayer.isPlaying) // If video is playing then...
+        {
+            // Pause Video
+            videoPlayer.Pause();
+        }
+        else
+        {
+            // Play Video
+            videoPlayer.Play();
+        }
 
     }
 
     public void Exit_Video()
+    {
+        SceneManager.LoadScene("UI_WaitingRoom");
+    }
+
+    public void Restart_Video()
+    {
+        videoPlayer.Stop(); // Stops the video and not pause?
+        videoPlayer.Play(); // Then start the video again from the beginning.
+    }
+
+    public void OpenCloseBookmarksTab()
+    {
+        if(!isBookmarksOpen) 
+        {
+            // Open Tab
+        }
+        else
+        {
+            // Close tab
+        }
+
+    }
+
+    public void skipFwd()
+    {
+
+    }
+
+    public void skipBwd()
     {
 
     }
